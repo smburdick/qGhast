3edd4607ded1609442ba44ab8251dfd92b2e54ac
Sam Burdick <samuelmburdick@gmail.com>
2024-02-29 18:44:19 -0800

    Add enemies and AI, maze gen

--- src/main.js
+++ src/main.js
@@ -1,12 +1,107 @@
 import kaboom from "kaboom"
 
-const k = kaboom()
+kaboom()
 
-k.loadSprite("bean", "sprites/bean.png")
+loadSprite("pc", "sprites/pc.png")
+loadSprite("x", "sprites/x-error.png")
+loadSprite("z", "sprites/z-error.png")
 
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
+setBackground(141, 183, 255)
+
+const SPEED = 320
+const ENEMY_SPEED = 160
+const BULLET_SPEED = 800
+
+// Add player game object
+const player = add([
+	sprite("pc"),
+	pos(80, 80),
+	area(),
+	anchor("center"),
 ])
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+const enemy = add([
+	sprite("x"),
+	pos(width() - 80, height() - 80),
+	anchor("center"),
+	// This enemy cycle between 3 states, and start from "idle" state
+	state("move", [ "idle", "attack", "move" ]),
+])
+
+// Run the callback once every time we enter "idle" state.
+// Here we stay "idle" for 0.5 second, then enter "attack" state.
+enemy.onStateEnter("idle", async () => {
+	await wait(0.5)
+	enemy.enterState("attack")
+})
+
+// When we enter "attack" state, we fire a bullet, and enter "move" state after 1 sec
+enemy.onStateEnter("attack", async () => {
+
+	// Don't do anything if player doesn't exist anymore
+	if (player.exists()) {
+
+		const dir = player.pos.sub(enemy.pos).unit()
+
+		add([
+			pos(enemy.pos),
+			move(dir, BULLET_SPEED),
+			rect(12, 12),
+			area(),
+			offscreen({ destroy: true }),
+			anchor("center"),
+			color(BLUE),
+			"bullet",
+		])
+
+	}
+
+	await wait(1)
+	enemy.enterState("move")
+
+})
+
+enemy.onStateEnter("move", async () => {
+	await wait(2)
+	enemy.enterState("idle")
+})
+
+// Like .onUpdate() which runs every frame, but only runs when the current state is "move"
+// Here we move towards the player every frame if the current state is "move"
+enemy.onStateUpdate("move", () => {
+	if (!player.exists()) return
+	const dir = player.pos.sub(enemy.pos).unit()
+	enemy.move(dir.scale(ENEMY_SPEED))
+})
+
+// Taking a bullet makes us disappear
+player.onCollide("bullet", (bullet) => {
+	destroy(bullet)
+	destroy(player)
+	addKaboom(bullet.pos)
+})
+
+// Register input handlers & movement
+onKeyDown("left", () => {
+	player.move(-SPEED, 0)
+})
+
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+})
+
+onKeyDown("up", () => {
+	player.move(0, -SPEED)
+})
+
+onKeyDown("down", () => {
+	player.move(0, SPEED)
+})
+
+export default function generateMaze () {
+	const nmg = require('node-maze-generator');
+	const generator = new nmg.generators.generator([{ generator: nmg.generators.maze, options: { width: 10, height: 10 } }]);
+	return nmg.renderer(generator);
+}
+
+const maze = generateMaze()
