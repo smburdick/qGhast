658912845709da6a8dcf5e057137ee91a7fab179
Sam Burdick <samuelmburdick@gmail.com>
2024-02-29 19:39:47 -0800

    Add randomly generated enemies and other assets

--- src/main.js
+++ src/main.js
@@ -15,63 +15,81 @@ const BULLET_SPEED = 800
 // Add player game object
 const player = add([
 	sprite("pc"),
+	scale(0.5),
 	pos(80, 80),
 	area(),
 	anchor("center"),
 ])
 
-const enemy = add([
-	sprite("x"),
-	pos(width() - 80, height() - 80),
-	anchor("center"),
-	// This enemy cycle between 3 states, and start from "idle" state
-	state("move", [ "idle", "attack", "move" ]),
-])
-
-// Run the callback once every time we enter "idle" state.
-// Here we stay "idle" for 0.5 second, then enter "attack" state.
-enemy.onStateEnter("idle", async () => {
-	await wait(0.5)
-	enemy.enterState("attack")
-})
-
-// When we enter "attack" state, we fire a bullet, and enter "move" state after 1 sec
-enemy.onStateEnter("attack", async () => {
-
-	// Don't do anything if player doesn't exist anymore
-	if (player.exists()) {
-
-		const dir = player.pos.sub(enemy.pos).unit()
-
-		add([
-			pos(enemy.pos),
-			move(dir, BULLET_SPEED),
-			rect(12, 12),
+// Each second, there is a 1 in 10 chance of spawning a new enemy
+loop(1, () => {
+	const errorRate = 0.1;
+	const poll = Math.round(rand(1 / errorRate));
+	console.log(poll)
+	if (poll === 0) {
+		const xError = add([
+			sprite("z"),
+			scale(0.5),
+			pos(rand(0, width()), rand(0, height())),
 			area(),
-			offscreen({ destroy: true }),
 			anchor("center"),
-			color(BLUE),
-			"bullet",
+			state("move", [ "idle", "attack", "move" ]),
 		])
-
+		// Run the callback once every time we enter "idle" state.
+		// Here we stay "idle" for 0.5 second, then enter "attack" state.
+		xError.onStateEnter("idle", async () => {
+			await wait(0.5)
+			xError.enterState("attack")
+		})
+		// When we enter "attack" state, we fire a bullet, and enter "move" state after 1 sec
+		xError.onStateEnter("attack", async () => {
+			// Don't do anything if player doesn't exist anymore
+			if (player.exists()) {
+				const dir = player.pos.sub(xError.pos).unit()
+				add([
+					pos(xError.pos),
+					move(dir, BULLET_SPEED),
+					rect(12, 12),
+					area(),
+					offscreen({ destroy: true }),
+					anchor("center"),
+					color(BLUE),
+					"bullet",
+				])
+			}
+			await wait(1)
+			xError.enterState("move")
+		})
+		xError.onStateEnter("move", async () => {
+			await wait(2)
+			xError.enterState("idle")
+		})
+		// Like .onUpdate() which runs every frame, but only runs when the current state is "move"
+		// Here we move towards the player every frame if the current state is "move"
+		xError.onStateUpdate("move", () => {
+			if (!player.exists()) return
+			const dir = player.pos.sub(xError.pos).unit()
+			xError.move(dir.scale(ENEMY_SPEED))
+		})
+	} else if (poll === 1) {
+		const zError = add([
+			sprite("x"),
+			scale(0.5),
+			pos(rand(0, width()), rand(0, height())),
+			area(),
+			anchor("center"),
+			state("move", [ "idle", "attack", "move" ]),
+		]);
+		zError.onStateEnter("move", async () => {
+			await wait(2)
+			zError.enterState("idle")
+		})
+		zError.onStateUpdate("move", () => {
+			if (!player.exists()) return
+			const dir = player.pos.sub(zError.pos).unit()
+			zError.move(dir.scale(ENEMY_SPEED))
+		})
 	}
-
-	await wait(1)
-	enemy.enterState("move")
-
-})
-
-enemy.onStateEnter("move", async () => {
-	await wait(2)
-	enemy.enterState("idle")
-})
-
-// Like .onUpdate() which runs every frame, but only runs when the current state is "move"
-// Here we move towards the player every frame if the current state is "move"
-enemy.onStateUpdate("move", () => {
-	if (!player.exists()) return
-	const dir = player.pos.sub(enemy.pos).unit()
-	enemy.move(dir.scale(ENEMY_SPEED))
 })
 
 // Taking a bullet makes us disappear
@@ -87,6 +105,7 @@ onKeyDown("left", () => {
 })
 
 onKeyDown("right", () => {
+	// player.flipX(true)
 	player.move(SPEED, 0)
 })
 
@@ -101,7 +120,7 @@ onKeyDown("down", () => {
 export default function generateMaze () {
 	const nmg = require('node-maze-generator');
 	const generator = new nmg.generators.generator([{ generator: nmg.generators.maze, options: { width: 10, height: 10 } }]);
-	return nmg.renderer(generator);
+	return new nmg.renderer(generator);
 }
 
 const maze = generateMaze()
